da35d87da89948437c5d8262d73700cc6eb11679
worapit <drm.wmwork@gmail.com>
2021-11-30 16:30:59 +0700

    complete

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -9,7 +9,17 @@ int main(){
 	int id[] = {123,196,225,329,420,451,515,666,690,787};
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
 
-    //Write your code here.
+    double GPA;
+    cout << "Please input GPA: ";
+	cin >> GPA;
+
+	int i = 0;
+	while(i < 10){
+		if(gpa[i] >= GPA){
+			cout << id[i] <<" "<< name[i] <<" "<< gpa[i] << "\n";
+		}
+		i++;
+	}
 		
 	return 0;
 }
